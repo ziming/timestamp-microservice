d23bb3153b47c445c4da0b3fe4fb4ea6cd0ce88e
Ziming Lan <ziming.lan@gmail.com>
2017-05-31 15:33:40 +0000

    v1 commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const moment = require('moment');
+
+// create our Express app
+const app = express();
+
+// In Cloud9 use port 8080
+
+// To run your application run the command node server.js in your console.
+app.get('/:dateInput', (req, res) => {
+    
+    // check if date param is unix time stamp or natural language date
+    // https://timestamp-ms.herokuapp.com/December%2015,%202015
+    // https://timestamp-ms.herokuapp.com/1450137600
+    
+    const dateInput = req.params.dateInput;
+    
+    // if (isNaN(dateInput)) {
+    //     dateInput = dateInput.replace('%20', ' ');
+    // } else {
+    //     // 1450137600
+    //     dateInput *= 1000;
+    // }
+    
+    const givenDate = moment(dateInput, ['X', 'MMMM D, YYYY']);
+    
+    if (givenDate.isValid()) {
+        
+        res.json({ 'unix': givenDate.format('X'), 'natural': givenDate.format('MMMM D, YYYY') });
+        return;
+    }
+        
+    res.json({ 'unix': null, 'natural': null });
+        
+    
+    
+});
+
+app.listen(8080, function () {
+  console.log('Timestamp app listening on port 8080!')
+});
